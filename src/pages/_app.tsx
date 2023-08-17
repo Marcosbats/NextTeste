44cc06379a9aac7abb1ce89a40f80dc33e35b5b9
Marcos Batista dos Santos <122176540+Marcosbats@users.noreply.github.com>
2023-08-17 10:01:32 -0300

    huddle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-//import './styles/global.scss'
-import type { AppProps } from 'next/app'
+//https://next-teste-mh74.vercel.app/
+import { AppProps } from 'next/app'
+import '../styles/global.scss'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+import { ThirdwebProvider, metamaskWallet,  coinbaseWallet,  walletConnect } from "@thirdweb-dev/react"
+import { Polygon } from "@thirdweb-dev/chains";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component, pageProps: { session, ...pageProps }
+}: AppProps) {
+   return (
+      <ThirdwebProvider
+        supportedWallets={[
+          metamaskWallet(),
+          coinbaseWallet(),
+          walletConnect({
+            projectId: "08b445ecbd8f782a4634a4efef0b700c"
+          }),
+        ]}
+        activeChain={Polygon}
+        autoConnect={false}>
+        <ToastContainer autoClose={3000}/>
+        <Component {...pageProps}/>
+      </ThirdwebProvider>
+  )
 }
